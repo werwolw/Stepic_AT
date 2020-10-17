f693c851a2238d8de7651abde0e8202b84604ea1
KN <komrad-shiryaev@yandex.ru>
2020-10-17 23:13:30 +0500

    Прочел послание инопланетян! 6.3

--- venv/section3/lesson6_step3.py
+++ venv/section3/lesson6_step3.py
@@ -1,14 +1,20 @@
+from telnetlib import EC
+
 import pytest
 from selenium import webdriver
 import time
 import math
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 @pytest.fixture(scope="function")
 def browser():
-    answer = math.log(int(time.time()))
+    # answer = math.log(int(time.time()))
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
@@ -23,11 +29,19 @@ def browser():
     "https://stepik.org/lesson/236903/step/1",
     "https://stepik.org/lesson/236904/step/1",
     "https://stepik.org/lesson/236905/step/1"
-    ])
+])
 def test_search_Correct(browser, site):
     browser.get(site)
-    browser.find_element_by_tag_name("textarea")
-    answer = math.log(int(time.time()))
+    # browser.find_element_by_css_selector(".textarea")
+    answer = str(math.log(int(time.time())))
     input = browser.find_element_by_tag_name("textarea")
     input.send_keys(answer)
+    button = browser.find_element_by_css_selector(".submit-submission")
+    button.click()
+    #text_el = WebDriverWait(browser, 10).until(
+    #    EC.text_to_be_present_in_element((By.CLASS_NAME, "smart-hints__hint")))
+    time.sleep(2)
+    text_el = browser.find_element_by_css_selector(".smart-hints__hint")
+    assert "Correct!" == text_el.text, "NOT CORRECT!"
+
     # нужно добавить сравнение сообщения с Correct
